eebcc6620203187745b60358dedb2da64d698812
ghostypods <israelafolabi11@gmail.com>
2024-08-03 00:08:21 -0500

    Fix matching columns don't trigger end game

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def create_board():
-    board = [[" ", " ", " ",],
-             [" ", " ", " ",],
-             [" ", " ", " ",]]
-
+    board = [[" ", " ", " "],
+             [" ", " ", " "],
+             [" ", " ", " "]]
     return board
 
 
@@ -10,25 +9,23 @@ def display_board(board):
     row_count = 0
     for row in board:
         print(" | ".join(row))
-
         if row_count < 2:
-            print("- + - + -")
-
+            print('- + - + -')
         row_count += 1
 
 
 def player_move(board, player):
     while True:
         try:
-            move = input(f"Player {player}, select your move (row and column): ")
+            move = input(f"Player {player}, make your move (row and column): ")
             row, col = map(int, move.split())
             if board[row][col] == " ":
                 board[row][col] = player
                 break
             else:
-                print("This spot is taken")
+                print("This spot is taken, try again.")
         except (ValueError, IndexError):
-            print("Invalid move. Please select a row and column between 0 and 2")
+            print("Invalid value. Please enter numbers in between 0 and 2 (e.g. 0 2)")
 
 
 def check_winner(board, player):
@@ -36,13 +33,14 @@ def check_winner(board, player):
         if all(r == player for r in row):
             return True
 
-    for col in board:
-        if all(c == player for c in col):
+    for col in range(len(board)):
+        if all(board[row][col] == player for row in range(len(board))):
             return True
 
-    if all(board[i][i] == player for i in range(3)):
+    if all(board[i][i] == player for i in range(len(board))):
         return True
-    if all(board[i][2-i] == player for i in range(3)):
+
+    if all(board[i][2-i] == player for i in range(len(board))):
         return True
 
 
@@ -53,20 +51,22 @@ def check_draw(board):
 
 def main():
     board = create_board()
-    current_player = 'X'
+    current_player = "X"
 
-    gamie_is_on = True
-    while gamie_is_on:
+    game_is_on = True
+    while game_is_on:
         display_board(board)
         player_move(board, current_player)
 
         if check_winner(board, current_player):
-            print(f"Player {current_player} has won!")
-            gamie_is_on = False
+            display_board(board)
+            print(f"Player {current_player} wins")
+            game_is_on = False
 
         if check_draw(board):
-            print(f"Its a draw!")
-            gamie_is_on = False
+            display_board(board)
+            print("It's a draw")
+            game_is_on = False
 
         current_player = "O" if current_player == "X" else "X"
 
